87839d956161ab822d435a9303813a43791b11bc
William Sadler <william.sadler@tn.gov>
2019-09-13 07:49:37 -0500

    foxed stuff

---
+++ deDupeTOMIS/WhoAmI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace deDupeTOMIS
+{
+    public class WhoAmI
+    {
+        private static String Uid { get; set; }
+        private static String Pwd { get; set; }
+
+        private WhoAmI() { }
+        public static readonly WhoAmI whoAmI = new WhoAmI();
+
+        public bool Impersonate()
+        {
+            MessageBox.Show(string.Format("Logged In As @", Uid), "Successful Impersonation");
+            return true;
+        }
+    }
+}
